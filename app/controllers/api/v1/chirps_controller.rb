76f0be65f568a9cdacf9a9086b05ff4069ca3e0b
nichia <nichia@gmail.com>
2020-02-16 10:05:29 -0500

    Create a new chirp

--- app/controllers/api/v1/chirps_controller.rb
+++ app/controllers/api/v1/chirps_controller.rb
@@ -1,9 +1,28 @@
 class Api::V1::ChirpsController < ApplicationController
   
-    # GET /shirps
+    # GET /chirps
     def index
       @chirps = Chirp.all
+      
       render json: @chirps
     end
 
+    # POST /chirps
+    def create
+      @chirp = Chirp.new(chirp_params)
+
+      if @chirp.save
+        render json: @chirp, status: :created
+      else
+        render json: @chirp.errors, status: :not_acceptable
+
+      end
+    end
+
+    private
+
+    # Only allow a trusted parameter "white list" through.
+    def chirp_params
+      params.require(:chirp).permit(:text)
+    end
 end
